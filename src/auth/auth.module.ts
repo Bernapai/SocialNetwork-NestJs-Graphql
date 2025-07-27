aa1dba70f275144e275215d746dd9527751eb74d
Bernapai <juanemeunier@gmail.com>
2025-07-27 17:01:51 -0300

    New Decorator : CurrentUser

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,26 @@
-import { Module } from '@nestjs/common';
-import { AuthService } from './services/auth.service';
+import { Module, forwardRef } from '@nestjs/common';
 import { AuthResolver } from './resolvers/auth.resolver';
+import { AuthService } from './services/auth.service';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { UsersModule } from 'src/users/users.module';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  providers: [AuthResolver, AuthService],
+  imports: [
+    PassportModule,
+    forwardRef(() => UsersModule),
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.get<string>('JWT_EXPIRES_IN'),
+        },
+      }),
+    }),
+  ],
+  providers: [AuthService, AuthResolver],
+  exports: [JwtModule],
 })
 export class AuthModule { }
