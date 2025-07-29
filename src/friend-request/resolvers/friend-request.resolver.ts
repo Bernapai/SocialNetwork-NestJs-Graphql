aa6e982c76ba43489333be533f249ce123193f73
Bernapai <juanemeunier@gmail.com>
2025-07-29 16:05:37 -0300

    Add Resolver + Services for Friend-Request module

--- src/friend-request/resolvers/friend-request.resolver.ts
+++ src/friend-request/resolvers/friend-request.resolver.ts
@@ -3,33 +3,66 @@ import { FriendRequestService } from '../services/friend-request.service';
 import { FriendRequest } from '../entities/friend-request.entity';
 import { CreateFriendRequestInput } from '../dto/create-friend-request.input';
 import { UpdateFriendRequestInput } from '../dto/update-friend-request.input';
+import { UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { CurrentUser } from 'src/auth/decorators/currentUser.decorator';
+import { User } from 'src/users/entities/user.entity';
+import { ForbiddenException } from '@nestjs/common';
 
 @Resolver(() => FriendRequest)
 export class FriendRequestResolver {
   constructor(private readonly friendRequestService: FriendRequestService) { }
 
+  @UseGuards(JwtAuthGuard)
   @Mutation(() => FriendRequest)
-  createFriendRequest(@Args('createFriendRequestInput') createFriendRequestInput: CreateFriendRequestInput) {
+  createFriendRequest(@Args('createFriendRequestInput') createFriendRequestInput: CreateFriendRequestInput): Promise<FriendRequest> {
     return this.friendRequestService.create(createFriendRequestInput);
   }
 
-  @Query(() => [FriendRequest], { name: 'friendRequest' })
-  findAll() {
+  @UseGuards(JwtAuthGuard)
+  @Query(() => [FriendRequest], { name: 'friendRequests' })
+  findAll(): Promise<FriendRequest[]> {
     return this.friendRequestService.findAll();
   }
 
+  @UseGuards(JwtAuthGuard)
   @Query(() => FriendRequest, { name: 'friendRequest' })
-  findOne(@Args('id', { type: () => Int }) id: number) {
+  findOne(@Args('id', { type: () => String }) id: string): Promise<FriendRequest> {
     return this.friendRequestService.findOne(id);
   }
 
-  @Mutation(() => FriendRequest)
-  updateFriendRequest(@Args('updateFriendRequestInput') updateFriendRequestInput: UpdateFriendRequestInput) {
-    return this.friendRequestService.update(updateFriendRequestInput.id, updateFriendRequestInput);
+  @UseGuards(JwtAuthGuard)
+  @Query(() => [FriendRequest], { name: 'myFriendRequests' })
+  async findMyFriendRequests(@CurrentUser() currentUser: User): Promise<FriendRequest[]> {
+    const friendRequests = await this.friendRequestService.findAll();
+    return friendRequests.filter(request =>
+      request.receiverId.toString() === currentUser._id.toString()
+    );
   }
 
+  @UseGuards(JwtAuthGuard)
   @Mutation(() => FriendRequest)
-  removeFriendRequest(@Args('id', { type: () => Int }) id: number) {
+  async updateFriendRequest(
+    @Args('id', { type: () => String }) id: string,
+    @Args('updateFriendRequestInput') updateFriendRequestInput: UpdateFriendRequestInput,
+  ): Promise<FriendRequest> {
+    return this.friendRequestService.update(id, updateFriendRequestInput);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Mutation(() => FriendRequest)
+  async removeFriendRequest(
+    @CurrentUser() currentUser: User,
+    @Args('id', { type: () => String }) id: string,
+  ): Promise<FriendRequest> {
+    const friendRequest = await this.friendRequestService.findOne(id);
+
+    if (friendRequest.senderId.toString() !== currentUser._id.toString()) {
+      throw new ForbiddenException('Solo puedes eliminar tus propias solicitudes de amistad');
+    }
+
     return this.friendRequestService.remove(id);
   }
+
+
 }
--- src/friend-request/services/friend-request.service.ts
+++ src/friend-request/services/friend-request.service.ts
@@ -1,26 +1,49 @@
 import { Injectable } from '@nestjs/common';
 import { CreateFriendRequestInput } from '../dto/create-friend-request.input';
 import { UpdateFriendRequestInput } from '../dto/update-friend-request.input';
+import { FriendRequest, FriendRequestDocument } from '../entities/friend-request.entity';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { NotFoundException } from '@nestjs/common';
 
 @Injectable()
 export class FriendRequestService {
-  create(createFriendRequestInput: CreateFriendRequestInput) {
-    return 'This action adds a new friendRequest';
+  constructor(
+    @InjectModel(FriendRequest.name)
+    private friendRequestModel: Model<FriendRequestDocument>,
+  ) { }
+
+  async create(createFriendRequestInput: CreateFriendRequestInput): Promise<FriendRequest> {
+    const createdFriendRequest = new this.friendRequestModel(createFriendRequestInput);
+    return createdFriendRequest.save();
   }
 
-  findAll() {
-    return `This action returns all friendRequest`;
+  async findAll(): Promise<FriendRequest[]> {
+    return this.friendRequestModel.find().exec();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} friendRequest`;
+  async findOne(id: string): Promise<FriendRequest> {
+    const friendRequest = await this.friendRequestModel.findById(id).exec();
+    if (!friendRequest) {
+      throw new NotFoundException('Friend Request not found');
+    }
+    return friendRequest;
   }
 
-  update(id: number, updateFriendRequestInput: UpdateFriendRequestInput) {
-    return `This action updates a #${id} friendRequest`;
+  async update(id: string, updateFriendRequestInput: UpdateFriendRequestInput): Promise<FriendRequest> {
+    const updatedFriendRequest = await this.friendRequestModel.findByIdAndUpdate(id, updateFriendRequestInput, { new: true }).exec();
+    if (!updatedFriendRequest) {
+      throw new NotFoundException('Friend Request not found');
+    }
+    return updatedFriendRequest;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} friendRequest`;
+  async remove(id: string): Promise<FriendRequest> {
+    const deletedFriendRequest = await this.friendRequestModel.findByIdAndDelete(id).exec();
+    if (!deletedFriendRequest) {
+      throw new NotFoundException('Friend Request not found');
+    }
+    return deletedFriendRequest;
   }
+
 }
